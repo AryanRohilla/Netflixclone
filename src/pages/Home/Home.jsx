86ccf7a085436266f46aced9eb50cbbf0120cd94
AryanRohilla <aryanrohilla369@gmail.com>
2025-08-17 22:37:13 +0530

    second commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
         <img src={hero_banner} alt="" className='banner-img'/>
         <div className="hero-caption">
             <img src={hero_title} alt="" className='caption-img'/>
-            <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
-            
+            <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy 2.</p>
+
         </div>
       </div>
     </div>
